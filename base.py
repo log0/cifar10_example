c21433ddcb99bd9117efdcdbab7564d563265d1e
Eric Chio <ckieric@gmail.com>
2016-01-03 23:16:32 -0800

    Basic red spot detection (very basic object tracking). Does not work well.

--- base.py
+++ base.py
@@ -14,18 +14,23 @@ from sklearn.cross_validation import *
 from sklearn.metrics import *
 from sklearn.preprocessing import LabelEncoder
 
+from PIL import Image, ImageDraw
+
+def to_int_array(a):
+    return list(map(lambda i: int(i), a))
+
 TRAIN_DIR = 'data/train/*'
-TRAIN_LABELS = 'data/trainLabels.csv'
+TRAIN_LABELS = 'data/labels.csv'
 R_DIGITS = re.compile('\d+')
 N_COUNT = 10000
 
 reader = csv.reader(open(TRAIN_LABELS))
-reader.__next__()
 
-train_labels_map = {i[0]: i[1] for i in reader}
+train_labels_map = {i[0]: to_int_array(i[1:]) for i in reader}
 train_labels = []
 train_data = []
 train_files = glob.glob(TRAIN_DIR)
+ids = []
 for i, train_file in enumerate(train_files):
     if i >= N_COUNT: break
 
@@ -35,24 +40,14 @@ for i, train_file in enumerate(train_files):
     file_id = R_DIGITS.findall(train_file)[0]
     label = train_labels_map[file_id]
     train_labels.append(label)
+    ids.append(file_id)
 train_data = np.array(train_data)
 train_data = np.transpose(train_data, axes = (0, 3, 1, 2))
 train_data = train_data / 255
-
 train_labels = np.array(train_labels)
-train_labels_encoder = LabelEncoder()
-train_labels = train_labels_encoder.fit_transform(train_labels)
-n_classes = len(set(train_labels))
+train_labels = train_labels / 32
 
-raw_train_labels = train_labels
-train_labels = np_utils.to_categorical(raw_train_labels)
-
-X_train, X_cv, Y_train, Y_cv, raw_train_labels_train, raw_train_labels_cv = \
-        train_test_split(train_data, train_labels, raw_train_labels, test_size = 0.20)
-
-print(train_data.shape)
-print(train_labels.shape)
-print(raw_train_labels_train)
+X_train, X_cv, Y_train, Y_cv, ids_train, ids_cv = train_test_split(train_data, train_labels, ids, test_size = 0.20)
 
 model = Sequential()
 model.add(Convolution2D(32, 3, 3, border_mode = 'same', input_shape = (3, 32, 32)))
@@ -73,16 +68,30 @@ model.add(Flatten())
 model.add(Dense(512))
 model.add(Activation('relu'))
 model.add(Dropout(0.25))
-model.add(Dense(n_classes))
+model.add(Dense(4))
 model.add(Activation('softmax'))
 
 sgd = SGD(lr=0.1, decay=1e-7, momentum=0.2, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd)
 
 model.fit(X_train, Y_train, nb_epoch = 20, batch_size = 50, show_accuracy = True, verbose = 1, validation_split = 0.05)
-Y_cv_pred = model.predict_classes(X_cv, batch_size = 50, verbose = 1)
+Y_cv_pred = model.predict(X_cv, batch_size = 50, verbose = 1)
 
-print('Y_cv: ', raw_train_labels_cv[:100])
-print('Y_cv_pred: ', Y_cv_pred[:100])
+print('Y_cv.shape: ', Y_cv.shape)
+print('Y_cv_pred.shape: ', Y_cv_pred.shape)
 
-print('Accuracy score: ', accuracy_score(Y_cv_pred, raw_train_labels_cv))
+print('Y_cv: ', Y_cv[:20] * 32)
+print('Y_cv_pred: ', np.round(Y_cv_pred[:20] * 32))
+
+Y_cv *= 32
+Y_cv_pred *= 32
+
+for i, id in enumerate(ids_cv):
+    filepath = 'data/train/%s.png' % (id)
+    image = Image.open(filepath)
+    draw = ImageDraw.Draw(image)
+    xys = ((Y_cv_pred[i][0], Y_cv_pred[i][1]), (Y_cv_pred[i][2], Y_cv_pred[i][3]))
+    draw.ellipse(xys, fill = 'blue')
+
+    save_filepath = 'data/predictions/%s.png' % (id)
+    image.save(save_filepath)
---
+++ generate_data.py
@@ -0,0 +1,63 @@
+import os
+import re
+import csv
+import glob
+import numpy as np
+import random
+import scipy
+import scipy.ndimage
+
+from PIL import Image, ImageDraw
+
+def random_bounding_circle():
+    IMAGE_WIDTH = 32
+    IMAGE_HEIGHT = 32
+
+    MAX_RADIUS = 5
+
+    x0 = random.randint(0, IMAGE_WIDTH - MAX_RADIUS)
+    y0 = random.randint(0, IMAGE_HEIGHT - MAX_RADIUS)
+    radius = random.randint(1, MAX_RADIUS)
+    x1 = x0 + radius
+    y1 = y0 + radius
+
+    return ((x0, y0), (x1, y1))
+
+R_DIGITS = re.compile('\d+')
+INPUT_DIR = 'data/source/*'
+OUTPUT_DIR = 'data/train/'
+LABELS_PATH = 'data/labels.csv'
+
+N_COUNT = 10000
+
+source_data = []
+source_files = glob.glob(INPUT_DIR)
+
+train_data = {}
+labels = {}
+for i, source_file in enumerate(source_files):
+    if i >= N_COUNT: break
+    file_id = R_DIGITS.findall(source_file)[0]
+
+    image = Image.open(source_file)
+    draw = ImageDraw.Draw(image)
+
+    x0y0x1y1 = random_bounding_circle()
+    draw.ellipse(x0y0x1y1, fill = 'red')
+
+    (x0, y0), (x1, y1) = x0y0x1y1
+
+    train_data[file_id] = image
+    labels[file_id] = [x0, y0, x1, y1]
+
+for file_id, image in train_data.items():
+    filename = os.path.join(OUTPUT_DIR, str(file_id) + '.png')
+    image.save(filename)
+
+# Save labels data
+with open(LABELS_PATH, 'w') as f_labels:
+    writer = csv.writer(f_labels)
+    for file_id, label in labels.items():
+        row = [file_id]
+        row.extend(label)
+        writer.writerow(row)
